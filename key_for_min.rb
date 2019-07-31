c210756cefa0e691232941fc9005feb9fce0098a
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-31 19:31:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  selected_key = nil
+  selected_value = nil
+  name_hash.each do |key, value|
+    if selected_key == nil
+      selected_key = key
+      selected_value = value
+    elsif value < selected_value
+      selected_key = key
+      #{table: 85, chair: 25, mattress: 450}
+    end
+  end
+  selected_key
 end
